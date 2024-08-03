2dde5ffa68f79b646ce2db597ac6bcc7a9601209
donisos1146 <donisos1146@gmail.com>
2024-08-03 11:57:23 +0200

    Update GetLastBlock.lua
    Changed api url to a working one

--- GetLastBlock.lua
+++ GetLastBlock.lua
@@ -1,9 +1,11 @@
 --[[
 	Returns the last block mined.
+	PreviousBlock["data"]["hash"]
 --]]
 
 local hs = game:GetService("HttpService")
-local url = "http://btc.blockr.io/api/v1/block/info/last"
+--local url = "http://btc.blockr.io/api/v1/block/info/last"
+local url = "https://api.blockcypher.com/v1/btc/main"
 
 return function()
 	local data = hs:GetAsync(url, true)
