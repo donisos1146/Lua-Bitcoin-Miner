fac803be81860b8e05fac5949a74e864145cbcc9
donisos1146 <donisos1146@gmail.com>
2024-08-03 12:16:41 +0200

    Update GetLastBlock.lua

--- GetLastBlock.lua
+++ GetLastBlock.lua
@@ -3,7 +3,7 @@
 --]]
 
 local hs = game:GetService("HttpService")
-local url = "http://btc.blockr.io/api/v1/block/info/last"
+local url = "https://api.blockcypher.com/v1/btc/main"
 
 return function()
 	local data = hs:GetAsync(url, true)
